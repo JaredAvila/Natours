56cd545f491322e79bc58b669be0369fd9d242b3
Jared Avila <jared.c.avila@gmail.com>
2019-11-01 22:02:25 -0700

    adding middlewares

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
+// 1) MIDDLEWARE
+
+app.use(morgan('dev'));
 app.use(express.json());
+app.use((req, res, next) => {
+  req.requestTime = new Date().toDateString();
+  next();
+});
+
+// ----------> gets tours array from file for now
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-// returns 404 if isValid is false
+// << helper >> ------> returns 404 if isValid is false
 
 const dataIsValid = (isValid, res) => {
   if (!isValid) {
@@ -20,12 +30,13 @@ const dataIsValid = (isValid, res) => {
   }
 };
 
-// request response handlers
+// 2) ROUTE HANDLERS
 
 const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
+    requestTime: req.requestTime,
     data: {
       tours
     }
@@ -47,7 +58,6 @@ const getTour = (req, res) => {
 };
 
 const createTour = (req, res) => {
-  //   console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
@@ -85,7 +95,7 @@ const deleteTour = (req, res) => {
   });
 };
 
-// Routes
+// 3) ROUTES
 app
   .route('/api/v1/tours')
   .get(getAllTours)
@@ -96,6 +106,8 @@ app
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
+
+// 4) START SERVER
 
 const port = 3000;
 app.listen(port, () => {
