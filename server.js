a33e14a0525e7b4501af26ab97ae7a892f69ada0
Jared Avila <jared.c.avila@gmail.com>
2019-11-06 20:53:17 -0800

    added mongoose and connected to DB

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-app.listen(process.env.PORT || 3000, () => {
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('MongoDB Connected'));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
   console.log(`Server running on port ${process.env.PORT}...`);
 });
