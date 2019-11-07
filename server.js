d9d5336ce132b452222e7fe4aa0a52f634ace8df
Jared Avila <jared.c.avila@gmail.com>
2019-11-07 07:08:28 -0800

    updating controllers

--- server.js
+++ server.js
@@ -19,31 +19,6 @@ mongoose
   })
   .then(() => console.log('MongoDB Connected'));
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must hava a name'],
-    unique: true
-  },
-  price: { type: Number, required: [true, 'A tour must have a price'] },
-  rating: { type: Number, default: 4.5 }
-});
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Park Camper',
-  price: 997
-});
-
-testTour
-  .save()
-  .then(doc => {
-    console.log(doc);
-  })
-  .catch(err => {
-    console.log(err);
-  });
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${process.env.PORT}...`);
