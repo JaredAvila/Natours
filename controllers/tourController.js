bfc63334b8d9d949d834a1b88e89f872b068ed61
Jared Avila <jared.c.avila@gmail.com>
2019-11-13 09:39:55 -0800

    implemented pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -22,8 +22,31 @@ exports.getAllTours = async (req, res) => {
     } else {
       query = query.sort('-createdAt');
     }
+
+    // 3) Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    // 4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 5;
+    const skip = (page - 1) * limit;
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
+    query = query.skip(skip).limit(limit);
+
+    // EXECUTE QUERY
     const tours = await query;
 
+    // RETURN RESULTS
     res.status(200).json({
       status: 'success',
       results: tours.length,
