8f4dd30bd2a63d3051ebdc2217629d5b224e3658
Jared Avila <jared.c.avila@gmail.com>
2019-11-14 13:36:43 -0800

    working on error handling

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', err => {
+  console.log(err.name, err.message);
+  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
@@ -20,6 +28,14 @@ mongoose
   .then(() => console.log('MongoDB Connected'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server running on port ${process.env.PORT}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
