7284b4adce9d6fde3f2fde313c8a4ac1ff55f012
Jared Avila <jared.c.avila@gmail.com>
2019-12-05 12:29:53 -0800

    adding security

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,6 +12,30 @@ const signToken = id => {
   });
 };
 
+const createAndSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
+  //remove password from output
+  user.password = undefined;
+
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user
+    }
+  });
+};
+
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
@@ -23,15 +47,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     photo: req.body.photo
   });
 
-  const token = signToken(newUser._id);
-
-  res.status(201).json({
-    status: 'success',
-    token,
-    data: {
-      user: newUser
-    }
-  });
+  createAndSendToken(newUser, 201, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -49,11 +65,7 @@ exports.login = catchAsync(async (req, res, next) => {
     return next(new AppError('Incorrect email or password', 401));
   }
   // 3) if everything is ok send token to client
-  const token = signToken(user._id);
-  res.status(200).json({
-    status: 'success',
-    token
-  });
+  createAndSendToken(user, 200, res);
 });
 
 exports.protect = catchAsync(async (req, res, next) => {
@@ -170,9 +182,23 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   // 3) update changedPasswordAt prop for user
 
   // 4) Log the user in, send JWT
-  const token = signToken(user._id);
-  res.status(200).json({
-    status: 'success',
-    token
-  });
+  createAndSendToken(user, 200, res);
+});
+
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  // 1) get user
+  const user = await User.findById(req.user.id).select('+password');
+  // 2) check if posted password is correct
+  if (
+    !user ||
+    !(await user.correctPassword(req.body.password, user.password))
+  ) {
+    return next(new AppError('Please enter correct password.', 401));
+  }
+  // 3) update password
+  user.password = req.body.newPassword;
+  user.password2 = req.body.newPassowrd2;
+  await user.save();
+  // 4) log user in
+  createAndSendToken(user, 200, res);
 });
--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,14 @@ const APIFeatures = require('../utils/apiFeatures');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
 
@@ -22,6 +30,38 @@ exports.createUser = (req, res) => {
     message: 'This route is not yet defined'
   });
 };
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) create error is user POSTs password data
+  if (req.body.password || req.body.password2) {
+    return next(new AppError('This route is not for password updates', 400));
+  }
+
+  // 2) filtered out unwanted field names
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  //3) Update user doc
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser
+    }
+  });
+});
+
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
+});
 
 exports.updateUser = (req, res) => {
   res.status(500).json({
